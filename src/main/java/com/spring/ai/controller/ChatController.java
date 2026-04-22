54bad89809dca8ae9f51f1f57738d101cab186eb
Madu Biradar <madubiradar@gmail.com>
2026-04-22 23:07:54 +0530

    movie recommendation

--- src/main/java/com/spring/ai/controller/ChatController.java
+++ src/main/java/com/spring/ai/controller/ChatController.java
@@ -35,7 +35,14 @@ public class ChatController {
     @GetMapping("/movies")
     public List<MovieRecommendation> getMovieRecommendations(@RequestParam String genre, @RequestParam int count){
 
-        return chatClient.prompt()
+        return (List<MovieRecommendation>) chatClient.prompt()
+                .user(u -> u.text("""
+                 Recommend exactly {count} movies in the {genre} genre. For each movie, provide the accurate title, description, genre, rating, year, and actors.
+                The rating should be a number between 0 and 10, and the year should be the year of the movie released. The data should be absolutely accurate.
+                Everytime even the genre is same, the movies should be different.
+                """).param("count", count).param("genre", genre)
+                ).call()
+                .entity(List.class);
 
     }
 }
--- src/main/java/com/spring/ai/dtos/MovieRecommendation.java
+++ src/main/java/com/spring/ai/dtos/MovieRecommendation.java
@@ -2,11 +2,23 @@ package com.spring.ai.dtos;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class MovieRecommendation {
+
+    String title;
+    String description;
+    String genre;
+    String rating;
+    String year;
+    List<String> actors;
+
 }
